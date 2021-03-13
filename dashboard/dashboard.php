0c4f56d932ee4329a913f35c63f814bf92776f69
Huends-Jhoebert <huendahuenda20@gmail.com>
2021-03-13 23:20:54 +0800

    initialize pos system dashboard

---
+++ dashboard/dashboard.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<!--
+   This is a starter template page. Use this page to start your new project from
+   scratch. This page gets rid of all links and provides the needed markup only.
+   -->
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Gem's Avon | Dashboard</title>
+      <!-- Google Font: Source Sans Pro -->
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+      <!-- Font Awesome Icons -->
+      <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="dist/css/adminlte.min.css">
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+      <!-- Tempusdominus Bootstrap 4 -->
+      <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+      <!-- iCheck -->
+      <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+      <!-- JQVMap -->
+      <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="dist/css/adminlte.min.css">
+      <!-- overlayScrollbars -->
+      <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+      <!-- Daterange picker -->
+      <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+      <!-- summernote -->
+      <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+   </head>
+   <body class="hold-transition sidebar-mini">
+      <div class="wrapper">
+         <!-- Navbar -->
+         <nav class="main-header navbar navbar-expand navbar-white navbar-light" style="background-color:#083E52; border-bottom: none; justify-content: space-between;">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+               <li class="nav-item">
+                  <a class="nav-link" data-widget="pushmenu" href="#" role="button" style="color: #fff"><i class="fas fa-bars"></i></a>
+               </li>
+            </ul>
+            <!-- sign out -->
+            <a href="/root/index.php" class="small-box-footer btn btn-danger">Sign out <i class="fas fa-sign-out-alt"></i></a>
+         </nav>
+         <!-- /.navbar -->
+         <!-- Main Sidebar Container -->
+         <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color:#073546">
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link" style="border-bottom:none;">
+            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image elevation-3" style="opacity: .8;"> 
+            <span class="brand-text font-weight-light" style="display:inline-block;"></span>
+            </a>
+            <!-- Sidebar -->
+            <div class="sidebar">
+               <!-- Sidebar user panel (optional) -->
+               <div class="user-panel mt-3 pb-3 mb-3 d-flex" style="border-top:1px solid #fff; border-bottom:1px solid #fff; padding-top:1rem">
+                  <div class="image">
+                     <img src="dist/img/user8-128x128.jpg" class="img-circle elevation-2" alt="User Image">
+                  </div>
+                  <div class="info">
+                     <a href="#" class="d-block">MANAGER</a>
+                  </div>
+               </div>
+               <!-- Sidebar Menu -->
+               <nav class="mt-2">
+                  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                     <!-- Add icons to the links using the .nav-icon class
+                        with font-awesome or any other icon font library -->
+                     <li class="nav-item">
+                        <a href="#" class="nav-link active" style="background-color: #39D3B0">
+                           <i class="nav-icon fas fa-th"></i>
+                           <p>
+                              Dashboard
+                              <!-- <span class="right badge badge-danger">New</span> -->
+                           </p>
+                        </a>
+                        <a href="#" class="nav-link active" style="background-color: #083E52">
+                           <i class="nav-icon fas fa-pen"></i>
+                           <p>
+                              Product IS
+                              <!-- <span class="right badge badge-danger">New</span> -->
+                           </p>
+                        </a>
+                        <a href="#" class="nav-link active" style="background-color: #083E52">
+                           <i class="nav-icon fas fa-cash-register"></i>
+                           <p>
+                              Sales IS
+                              <!-- <span class="right badge badge-danger">New</span> -->
+                           </p>
+                        </a>
+                        <a href="#" class="nav-link active" style="background-color: #083E52">
+                           <i class="nav-icon fas fa-chart-area"></i>
+                           <p>
+                              Report Generation
+                              <!-- <span class="right badge badge-danger">New</span> -->
+                           </p>
+                        </a>
+                     </li>
+                  </ul>
+               </nav>
+               <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+         </aside>
+         <!-- Content Wrapper. Contains page content -->
+         <div class="content-wrapper main-content-white" style="background-color:#073546">
+            <!-- Content Header (Page header) -->
+            <div class="content-header" style="">
+               <div class="container-fluid">
+                  <div class="row mb-2">
+                     <div class="col-sm-6">
+                        <h1 class="m-0" style="color:#fff; font-size: 2rem">Dashboard</h1>
+                     </div>
+                     <!-- /.col -->
+                     <!-- /.col -->
+                  </div>
+                  <!-- /.row -->
+               </div>
+               <!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+            <!-- Main content -->
+            <section class="content" >
+               <div class="container-fluid">
+                  <!-- Small boxes (Stat box) -->
+                  <div class="row" style="justify-content: space-evenly;">
+                     <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-maroon"> 
+                           <div class="inner">
+                              <h3>75</h3>
+                              <p>Products</p>
+                           </div>
+                           <div class="icon">
+                              <i class="fas fa-shopping-bag"></i>
+                           </div>
+                           <a href="#" class="small-box-footer">Product IS <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                     </div>
+                     <!-- ./col -->
+                     <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                           <div class="inner">
+                              <h3>53</h3>
+                              <p>Sales</p>
+                           </div>
+                           <div class="icon">
+                           <i class="fas fa-cart-plus"></i>
+                           </div>
+                           <a href="#" class="small-box-footer">Sales IS <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                     </div>
+                     <!-- ./col -->
+                     <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-primary">
+                           <div class="inner">
+                              <h3>SALES</h3>
+                              <p>Report Generation</p>
+                           </div>
+                           <div class="icon">
+                           <i class="fas fa-chart-bar"></i>
+                           </div>
+                           <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                     </div>
+                     <!-- ./col -->
+                     <!-- <div class="col-lg-3 col-6">
+                        
+                        <div class="small-box bg-danger">
+                           <div class="inner">
+                              <h3>User</h3>
+                              <p>Sign Out</p>
+                           </div>
+                           <div class="icon">
+                           <i class="fas fa-sign-out-alt"></i>
+                           </div>
+                           <a href="#" class="small-box-footer">Now <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                     </div> -->
+                     <!-- ./col -->
+                  </div>
+                  <!-- /.row -->
+                  <!-- Main row -->
+               </div>
+               <!-- /.container-fluid -->
+            </section>
+         </div>
+         <!-- /.content-wrapper -->
+         <!-- Control Sidebar -->
+         <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+            <div class="p-3">
+               <h5>Title</h5>
+               <p>Sidebar content</p>
+            </div>
+         </aside>
+         <!-- /.control-sidebar -->
+         <!-- Main Footer -->
+         <footer class="main-footer" style="background-color: #083E52; border-top: none">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+               <!-- Anything you want -->
+            </div>
+            <!-- Default to the left -->
+            <strong>Copyright &copy; 2021 <a href="#">Gem's Avon POS SYSTEM</a>.</strong> All rights reserved.
+         </footer>
+      </div>
+      <!-- ./wrapper -->
+      <!-- REQUIRED SCRIPTS -->
+      <!-- jQuery -->
+      <!-- Bootstrap 4 -->
+      <!-- AdminLTE App -->
+      <script src="dist/js/adminlte.min.js"></script>
+      <!-- jQuery -->
+      <script src="plugins/jquery/jquery.min.js"></script>
+      <!-- jQuery UI 1.11.4 -->
+      <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+      <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+      <script>
+         $.widget.bridge('uibutton', $.ui.button)
+      </script>
+      <!-- Bootstrap 4 -->
+      <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <!-- ChartJS -->
+      <script src="plugins/chart.js/Chart.min.js"></script>
+      <!-- Sparkline -->
+      <script src="plugins/sparklines/sparkline.js"></script>
+      <!-- JQVMap -->
+      <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+      <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+      <!-- jQuery Knob Chart -->
+      <script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+      <!-- daterangepicker -->
+      <script src="plugins/moment/moment.min.js"></script>
+      <script src="plugins/daterangepicker/daterangepicker.js"></script>
+      <!-- Tempusdominus Bootstrap 4 -->
+      <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+      <!-- Summernote -->
+      <script src="plugins/summernote/summernote-bs4.min.js"></script>
+      <!-- overlayScrollbars -->
+      <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+      <!-- AdminLTE App -->
+      <script src="dist/js/adminlte.js"></script>
+      <!-- AdminLTE for demo purposes -->
+      <script src="dist/js/demo.js"></script>
+      <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+      <script src="dist/js/pages/dashboard.js"></script>
+   </body>
+</html>
