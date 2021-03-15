aa73cefa1b7e0e8200cce9bda02ee2e847d7245a
Huends-Jhoebert <huendahuenda20@gmail.com>
2021-03-15 14:46:44 +0800

    added pos logo

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -54,10 +54,11 @@
          <!-- Main Sidebar Container -->
          <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #083E52">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image elevation-3" style="opacity: .8;"> 
-            <span class="brand-text font-weight-light" style="display:inline-block;"></span>
-            </a>
+            <!-- Brand Logo -->
+         <a href="index3.html" class="brand-link">
+            <img src="dist/img/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light" style="font-family:'Montserrat', sans-serif;">GEM'S AVON</span>
+         </a>
             <!-- Sidebar -->
             <div class="sidebar">
                <!-- Sidebar user panel (optional) -->
